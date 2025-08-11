d8f7a578b5f592d02eb4a1c07a802147d0d802f2
allAboutJS <victoronah1914@gmail.com>
2025-08-11 12:14:01 +0100

    chore: add comment guides

--- src/server/server.js
+++ src/server/server.js
@@ -1,28 +1,31 @@
-require ('dotenv').config();
-const express = require ('express');
-const mongoose = require('mongoose');
+// Use es6 import style `import module from "path";`
+// Node.js require is old school and will throw errors
+
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const cookieParser = require('cookie-parser');
-const connectDB = require('./config/dbConn')
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/dbConn");
 connectDB();
-const registerRoutes = require('./routes/authRoutes')
-
-
+const registerRoutes = require("./routes/authRoutes");
 
 app.use(express.json());
-app.use(cors({
-  origin:" http://localhost:3000",
-  credentials: true
-}));
+app.use(
+	cors({
+		origin: " http://localhost:3000",
+		credentials: true,
+	}),
+);
 app.use(cookieParser());
 
-app.use('api/auth', registerRoutes);
- 
+app.use("api/auth", registerRoutes);
 
 const PORT = 5000 || process.env.PORT;
 
-
-
-app.listen ( PORT, () => {
-  console.log (`server is running on ${PORT}`)
-})
+// Make a function
+// export const startServer = () => app.listen(port, callback);
+// Import in the main.ts and call the function
+app.listen(PORT, () => {
+	console.log(`server is running on ${PORT}`);
+});
