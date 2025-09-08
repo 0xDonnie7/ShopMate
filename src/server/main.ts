e5b267bdfb35196d7ea237dc7627155f48cf3b83
Victor Onah <VICTORONAH1914@GMAIL.COM>
2025-09-08 15:43:10 +0100

    update: refactor server configuration

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import { configApp } from "./server.js" ;
+import { connectDB } from "./config/dbConn.js";
+
 const app = express();
+configApp(app);
 
-
-import { startServer } from "./server.js" ;
-
-startServer();
-
-
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
-);
+ViteExpress.listen(app, 3000, async () => {
+  await connectDB();
+  console.log("Server is listening on port 3000...")
+});
