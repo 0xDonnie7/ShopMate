be6171384261a68ada259844dabdefbbc37aade3
Victor Onah <VICTORONAH1914@GMAIL.COM>
2025-09-08 15:39:26 +0100

    update: refactor server configuration

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,32 +1,9 @@
-// Use es6 import style `import module from "path";`
-// Node.js require is old school and will throw errors
-
-import 'dotenv/config';
-import express from "express";
-const  app = express();
+import {config} 'dotenv';
 import cookieParser from "cookie-parser";
-import cors from "cors";
-import connectDB from "./config/dbConn";
-connectDB();
-import registerRoutes from "./Auth/routes/authRoutes";
+import authRouter from "./Auth/routes/authRoutes";
 
-app.use(express.json());
-app.use(
-	cors({
-		origin: " http://localhost:3000",
-		credentials: true,
-	}),
-);
-app.use(cookieParser());
-
-app.use("/api/auth", registerRoutes);
-
-const PORT = process.env.PORT || 5000;
-
-// Make a function
-export const startServer = () =>{ 
-	app.listen(	PORT, () => {
-	console.log(`server is running on ${PORT}`);
-
-})
+export const configApp = (app) => {
+	app.use(express.json());
+	app.use(cookieParser());
+	app.use("/api/auth", authRouter);
 };
